c26b01379bd97a76d07eafca1161f611b0c98d02
Rodolfoplj <161321092+Rodolfoplj@users.noreply.github.com>
2024-11-04 08:11:45 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'minecraft',
+    'qual e o boss do final do jogo?',
+    'ender dragon'
+)
+
+criaCartao(
+    'clash royale',
+    'qual carta tem mais HP?',
+    'golem de peda'
+)
+
+criaCartao(
+    'futebol',
+    'qual jogador com mais bola de ouro?',
+    'messi'
+)
+
+criaCartao(
+    'futebol',
+    'qual jogador com mais gols?',
+    'Pele'
+)
+
+criaCartao(
+    'fetebol',
+    'Vini jr',
+    '0'
+)
+
+criaCartao(
+    'futebol',
+    'nome da empresa de carros eletricos mais famosa do mundo',
+    'tesla'
+)
+
+criaCartao(
+    'carro',
+    'quem crio a ferrari',
+    'Enzo ferrari'
+)
+
+criaCartao(
+    'carro',
+    'quem crio a lamborguini',
+    'Ferrucio lamborguini'
+)
+
+
+criaCartao(
+    'carro',
+    'qual carro mais rapido do mundo',
+    'SSC tuatara'
+)
